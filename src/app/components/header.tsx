cd4534f36e39027ff379170a8d016d248e700431
NoahSignt <noahvass01@gmail.com>
2024-01-29 14:32:28 +1100

    style: initial header design

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -2,19 +2,24 @@ import Link from 'next/link';
 
 export default function Header() {
     return(
-        <div>
-            <Link href="/">
-                Home
-            </Link>
-            <Link href="/signin">
-                Signin
-            </Link>
-            <Link href="/signup">
-                Signup
-            </Link>
-            <Link href="/editor">
-                Editor
-            </Link>
+        <div className="header">
+            <div className="header-logo">
+                Narwrites
+            </div>
+            <div className="header-links">
+                <Link href="/">
+                    Home
+                </Link>
+                <Link href="/signin">
+                    Signin
+                </Link>
+                <Link href="/signup">
+                    Signup
+                </Link>
+                <Link href="/editor">
+                    Editor
+                </Link>
+            </div>
         </div>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import styles from "./page.module.css";
 import Header from "./components/Header";
-import addData from "@/lib/firebase/firestore/addData";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <>
       <Header />
-    </main>
+      <main className={styles.main}>
+      </main>
+    </>
   );
 }
